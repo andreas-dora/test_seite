54cbf6cba8e416ff827c1dc63564b3e6552f07bf
andreas-dora <andreas.dora@icloud.com>
2001-01-02 03:14:31 +0100

    uebungen

---
+++ newSites/radioButtons/sketch.js
@@ -0,0 +1,9 @@
+const message = document.getElementById('message');
+var orderForm = document.getElementById('orderForm');
+var radioBtns = orderForm.order;
+for(let i = 0; i < radioBtns.length; i++){
+  radioBtns[i].addEventListener('change', (e) => {
+    console.log(e.target.value);
+    message.innerText = e.target.value;
+  })
+} 
