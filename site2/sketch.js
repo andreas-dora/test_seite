75b6f5b708603474536b6cc0ecb54b83fb343be7
Andreas Dora <andreasdora@Andreass-MBP.fritz.box>
2019-09-21 05:20:08 +0200

    Baue Test2

---
+++ site2/sketch.js
@@ -0,0 +1,14 @@
+/*
+function setup(){
+    createCanvas(640,360);
+}
+
+function draw(){
+    background(0, 40, 60);
+
+    fill(0, 100, 200);
+    ellipseMode(CENTER);
+    ellipse(100, 100, 50, 50);
+
+}
+*/
