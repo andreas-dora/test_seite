7043431c2779265ad747d17f5051655f43597d18
andreas dora <andreas.dora@icloud.com>
2019-12-22 23:57:14 +0100

    Ich arbeite am copy to clipboard

--- Exercises/fromW3School/CopyToClipBord/sketch.js
+++ Exercises/fromW3School/CopyToClipBord/sketch.js
@@ -1,9 +1,17 @@
+var myBtn = document.getElementById("myBtn");
+myBtn.addEventListener("click", myFun);
 function myFun(){
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-
+  // var copyText = document.getElementById("myInput");
+  // copyText.select();
+  // copyText.setSelectionRange(0, 99999);
+  // var myTest = "Zauberei";
+  // myTest.select();
+  // alert("FOTZILLA");
+  var myZauber = document.getElementById("zauberei");
+  myZauber.setSelectionRange(0, 99999);
+  myZauber.value.select();
   document.execCommand("copy");
+  // document.execCommand("copy");
 
 
 }
