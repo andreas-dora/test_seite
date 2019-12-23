a75c37a39d0362a26b8be98e4a505703ddf4a18f
andreas dora <andreas.dora@icloud.com>
2019-12-23 03:03:37 +0100

    Copy to Clipboard ausgearbeitet

--- Exercises/fromW3School/CopyToClipBord/sketch.js
+++ Exercises/fromW3School/CopyToClipBord/sketch.js
@@ -1,17 +1,38 @@
-var myBtn = document.getElementById("myBtn");
-myBtn.addEventListener("click", myFun);
-function myFun(){
-  // var copyText = document.getElementById("myInput");
-  // copyText.select();
-  // copyText.setSelectionRange(0, 99999);
-  // var myTest = "Zauberei";
-  // myTest.select();
-  // alert("FOTZILLA");
-  var myZauber = document.getElementById("zauberei");
-  myZauber.setSelectionRange(0, 99999);
-  myZauber.value.select();
-  document.execCommand("copy");
-  // document.execCommand("copy");
+const myBtn = document.getElementById("myBtn");
+myBtn.addEventListener("click", standardFunction);
 
+const cpBtn = document.getElementById("cpBtn");
+cpBtn.addEventListener("click", myFun);
+
+///-----------------------Standard ---------------------------
+function standardFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
+
+//--------------------- My Funktion
+function myFun(){
+  var myCopy = document.getElementById("testFeld1");
+  const myHlp = document.createElement('textarea');
+  myHlp.value = myCopy.innerText;
+  myHlp.setAttribute('readonly', '');
+  myHlp.style.position = 'absolute'; 
+  myHlp.style.left = '-9999px';
+  document.body.appendChild(myHlp);
+  myHlp.select();
+  myHlp.setSelectionRange(0, 99999); /*For mobile devices*/
+  document.execCommand("copy");
+  alert("PENGO");
+  document.body.removeChild(myHlp);
 
 }
