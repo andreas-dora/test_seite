8576ad3bdb28ecc3eff1e460d36b5f733a33d7d8
andreas dora <andreas.dora@icloud.com>
2020-02-23 10:18:19 +0100

    neumorphism und javscript für multistep

---
+++ Exercises/fromW3School/forms/multiStepForm/sketch.js
@@ -0,0 +1,57 @@
+
+var currentTab = 0;
+showTab(currentTab);
+
+function showTab (n){
+  const x = document.getElementsByClassName("tab");
+  const pre = document.getElementById("preBtn");
+  const nxt = document.getElementById("nxtBtn");
+  x[n].getElementsByClassName.display = "block";
+  if(n == 0){
+    pre.style.display = "none";
+  } else {
+    pre.style.display = "inline";
+  }
+  if(n==(x.length-1)){
+    nxt.innerHTML = "Submit";
+  } else {
+    nxt.innerHTML = "Next";
+  }
+  fixStepIndicator(n)
+}
+
+function nxtPrv(n){
+  const x = document.getElementsByClassName("tab");
+  if(n ==1 && !ValidityState()) return false;
+  x[currentTab].style.display = "none";
+  currentTab = currentTab + n;
+  if(currentTab >= x.length){
+    document.getElementById("regForm").onsubmit();
+    return false;
+  }
+  showTab(currentTab);
+}
+
+function validateForm(){
+  var x,y,i,valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    for (i =0; i < y.lenght; i++){
+      if(y[i].value == ""){
+        y[i].className += " invalid";
+        valid = false;
+      }
+    }
+    if(valid){
+      document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid;
+  }
+ 
+function fixStepIndicator(n){
+  var i,x = document.getElementsByClassName("step");
+  for(i=0; i < x.length; i++){
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  x[n].className += " active";
+}
