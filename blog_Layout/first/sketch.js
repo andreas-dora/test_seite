d30efa1545a035ad559d7a1321d75212607aeabe
andreas dora <andreas.dora@icloud.com>
2020-07-23 15:54:22 +0200

    schön responsive

--- blog_Layout/first/sketch.js
+++ blog_Layout/first/sketch.js
@@ -1,3 +1,18 @@
+
+const modal = document.getElementById("id_modal_1");
+const img = document.getElementById("id_img_001");
+const modalImg = document.getElementById("img_1")
+const captionText =document.getElementById("id_caption_1");
+img.onclick =function () {
+  // alert("PFUI,PFUI");
+  modal.style.display = "block";
+  modalImg.src =this.src;
+  captionText = this.textContent;
+};
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function (){
+  modal.style.display = "none";
+};
 function navBar() {
   const navB = document.getElementById("id_topnav");
   if(navB.className === "topnav"){
@@ -6,3 +21,4 @@ function navBar() {
     navB.className = "topnav";
   }
 }
+
