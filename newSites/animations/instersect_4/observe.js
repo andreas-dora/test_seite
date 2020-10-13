12a09d84a198b508102d278f5ffad11c5c4ce93a
andreas-dora <andreas.dora@icloud.com>
2020-10-14 00:18:20 +0200

    insersection

---
+++ newSites/animations/instersect_4/observe.js
@@ -0,0 +1,21 @@
+
+const images = document.querySelectorAll('.anim');
+
+observer = new IntersectionObserver((entries) =>{
+  console.log(entries);
+
+  entries.forEach(entry => {
+    if(entry.intersectionRatio > 0){
+      entry.target.style.animation = `anim1 3s ${entry.target.dataset.delay} forwards ease-out`;
+    } 
+    else {
+      entry.target.style.animation = `none`;
+    }
+
+  })
+
+})
+
+images.forEach(image => {
+  observer.observe(image)
+})
