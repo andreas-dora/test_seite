3bcc5e8542d9387bcd889cf0e59dddc771a0e9b2
andreas-dora <andreas.dora@icloud.com>
2020-10-26 11:59:03 +0100

    es6 übung

---
+++ newSites/usefullScripts/es6/teil1/sketch.js
@@ -0,0 +1,13 @@
+const personalInfo = {
+  firstName: 'Andi',
+  lastName: 'Arbeit',
+  city: 'Offenbach',
+  state: 'Ding',
+  zipCode: '1234'
+};
+
+const {firstName: fn, lastName: ln} = personalInfo;
+
+console.log(`${fn} und auch ${ln}`);
+
+---
