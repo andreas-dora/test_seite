ad5da5a7c68be44df1aabd77681aa303d757340b
andreas-dora <andreas.dora@icloud.com>
2020-11-05 14:22:21 +0100

    regex formulieren

---
+++ newSites/usefullScripts/jsBasics/regex/regEx.js
@@ -0,0 +1,13 @@
+const myMobil = '+49 69 1234567';
+var rXp = /^\+?\d{2}\s\d{2,3}\s\d{5,7}$/;
+console.log(rXp.test(myMobil));
+// var meinKuchen = "Käsekuchen";
+// var eXp = /^K/;
+// console.log(eXp.test(meinKuchen));
+// const numberOne = '49 69 1234567';
+// const palindrom = "In Girum Imus Nocte Et Consumimur Igni";
+
+// const eXp = /............./;
+
+// console.log(eXp.test(numberOne));
+// console.log(eXp.test(palindrom));
