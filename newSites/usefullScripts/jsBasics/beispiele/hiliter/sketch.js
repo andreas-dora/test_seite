b0441caf73d727f9b8f0a12d4888d7f9c5f123ce
andreas-dora <andreas.dora@icloud.com>
2020-11-06 13:38:43 +0100

    working code hilighter

--- newSites/usefullScripts/jsBasics/beispiele/hiliter/sketch.js
+++ newSites/usefullScripts/jsBasics/beispiele/hiliter/sketch.js
@@ -1,23 +1,47 @@
-var inbox = document.querySelector('#inbox');
-var codeBox = document.querySelector('.codeBox');
-const start = document.getElementById('start');
-var firstCheck = /<(.*?)>/;
+// var rex =/\+?\d{2}\s\d{2}\s\d{5,7}/g
+// var text = "Eine Telefonnummer: 49 30 1234567 und die Nebenstelle: 49 30 98798798";
+// text = text.replace(rex, '<Telefonnummer durch überangedne Programierkunst verborgen>')
+// console.log(text);
+var output = document.querySelector('#output');
+var rex = /</;
+var workbench;
+var code = document.querySelector('.myCode');
+workbench= `<form action="">
+<label for="output">Output</label><br>
+<textarea name="" id="codeBox" cols="30" rows="10"></textarea>
+<button class="btn">Copy</button>
+</form>; `
+workbench = workbench.replace(/</g, '&lt;');
+workbench = workbench.replace(/>/g,'&gt;');
+workbench = workbench.replace(/(\s[a-z]{2,10})=/g,'<span class="atribut">$1</span>=');//Artibute
+workbench = workbench.replace(/>="(.*?)"/g,'>=<span class="val">"$1"</span>');//Values
+workbench = workbench.replace(/&gt;(.*?)&lt;/g,'&gt;<span class="text">$1</span>&lt;');//Values
+workbench = workbench.replace(/\/(\w+)&gt;/g,'/<span class="tag">$1</span>&gt');//Values
+workbench = workbench.replace(/&lt;(\w+)/g,'&lt;<span class="tag">$1</span>');//Values
+output.value = workbench;
+code.innerHTML = workbench;
+// console.log(typeof(workbench));  
 
-start.addEventListener('click', checkFun);
-let myNode = document.createTextNode('nknkkn');
-  // codeBox.appendChild(myNode);
-  // console.log(myNode.nodeValue);
+// console.log(code);
+// console.log(rex.test(code));
 
-function checkFun(){
-  
-  if(firstCheck.test(inbox.value)){
-    let workString = inbox.value;
+// var inbox = document.querySelector('#inbox');
 
-    console.log(workString);
-    // codeBox.value = inBox.value;
-  } else {
-    console.log('NIX');
-  }
+
+// start.addEventListener('click', checkFun);
+// let myNode = document.createTextNode('nknkkn');
+
+// function checkFun(){
+//   if(firstCheck.test(inbox.value)){
+//     let workString = inbox.value;
+
+//     console.log(workString);
+//   } else {
+//     console.log('NIX');
+//   }
+// }
+
+
 
   // if(inbox.value){
   //   console.log('cool');
@@ -26,6 +50,3 @@ function checkFun(){
   //   console.log('muschi');
   // }
 
-
-  
-}
