f5eab1c9cee4dcb34c39614fddff30adbd742d4b
andreas-dora <andreas.dora@icloud.com>
2020-12-15 22:00:05 +0100

    fullscreen Search

---
+++ newSites/navigation/fullScreen/sketch.js
@@ -0,0 +1,13 @@
+console.log("ALLES DA");
+const overlay = document.getElementById('navOverlay');
+const openBtn = document.getElementById('id_menu-btn');
+openBtn.addEventListener('click', openFun);
+const closeBtn = document.getElementById('id_closeBtn').addEventListener('click', closeFun);
+
+function openFun(){
+  overlay.style.height = "100%";
+}
+
+function closeFun(){
+  overlay.style.height = "0%";
+}
