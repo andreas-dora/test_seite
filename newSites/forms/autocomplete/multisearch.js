09df64b9aab87a5f4f07d41e872a06c5414e2350
Andreas Breitwieser <53184655+andreas-dora@users.noreply.github.com>
2021-03-10 01:22:32 +0100

    Create multisearch.js

---
+++ newSites/forms/autocomplete/multisearch.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form');
+const signinButton = document.querySelector('button#sign-in');
+
+form.addEventListener('submit', handleFormSubmission);                       
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  validatePassword();
+  form.reportValidity();
+  if (form.checkValidity() === false) {
+  } else {
+    // On a production site do form submission.
+    alert('Logging in!')
+    signinButton.disabled = 'true';
+  }
+}
